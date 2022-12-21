77bdab98c10fd4c1ccb29f7e7fa23c789b301bf1
Frédéric Bui <bui.frederic.pro@gmail.com>
2022-12-21 15:18:51 +0100

    model comment ref user gossip

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_135825) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_21_141501) do
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "gossip_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gossip_id"], name: "index_comments_on_gossip_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "gossips", force: :cascade do |t|
@@ -34,6 +44,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_21_135825) do
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
+  add_foreign_key "comments", "gossips"
+  add_foreign_key "comments", "users"
   add_foreign_key "gossips", "users"
   add_foreign_key "users", "cities"
 end
