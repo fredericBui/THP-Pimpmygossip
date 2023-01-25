b0ed69504c57ed7e2d0ce6ae4665cb99de7e5750
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-25 15:03:41 +0100

    debug likes

--- app/controllers/like_controller.rb
+++ app/controllers/like_controller.rb
@@ -1,12 +1,13 @@
 class LikeController < ApplicationController
     def like_dislike
         gossip = Gossip.find(params[:id])
-        if Like.find_by(liked: gossip)
-            puts "find like and destroy"
-            Like.find_by(liked: gossip).destroy
+        user = User.find(session[:user_id])
+        
+        if gossip.likes.find_by(user: user)
+            like = gossip.likes.find_by(user: user)
+            like.destroy
             redirect_to gossips_path
         else
-            puts "create a like"
             Like.create(user_id: session[:user_id],liked: gossip)
             redirect_to gossips_path
         end
@@ -14,12 +15,13 @@ class LikeController < ApplicationController
 
     def like_dislike_comment
         comment = Comment.find(params[:id])
-        if Like.find_by(liked: comment)
-            puts "find like and destroy"
-            Like.find_by(liked: comment).destroy
+        user = User.find(session[:user_id])
+        
+        if comment.likes.find_by(user: user)
+            like = comment.likes.find_by(user: user)
+            like.destroy
             redirect_to gossips_path
         else
-            puts "create a like"
             Like.create(user_id: session[:user_id],liked: comment)
             redirect_to gossips_path
         end
