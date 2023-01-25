f43a1d139c1a1b8eaa6c052cdec43e83e29a570a
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-25 10:40:20 +0100

    refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get :register, to: "register#index"
-  post :register, to: "register#create_user"
-  get :register_success , to: "register#success"
+  get :register, to: "users#new"
+  post :register, to: "users#create"
+  get :register_success , to: "users#success"
   
   #improve gossip comments and like
   resources :gossips do
